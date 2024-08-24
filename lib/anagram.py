402bf79e42b907694200c054ba16a9dd3b147b1b
yussufhh <0793477632g@gmail.com>
2024-08-24 14:46:12 +0300

    final-commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()  
+
+    def match(self, word_list):
+        sorted_word = sorted(self.word) 
+        matches = []
+        
+        for candidate in word_list:
+            if sorted_word == sorted(candidate.lower()):  
+                matches.append(candidate)
+        
+        return matches
---
+++ lib/index.py
@@ -0,0 +1,6 @@
+def add_numbers():
+ num1 = int(input("Enter the first number: "))
+ num2 = int(input("Enter the second number: "))
+ print("The sum of the two numbers is: ", num1 + num2)
+name = 'MOON LOVER'
+print(name.capitalize())
